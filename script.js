dedf7015e3f1e9ccd7fbe8268f01799397c148d6
Christina <chirochristina@gmail.com>
2024-03-28 19:31:22 -0700

    another approach

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    if (window.location.href.indexOf("autodownload") > -1) {
-        setTimeout(function() { 
-            window.open("https://example.com/docs/some_file.xlsx");
-        }, 3000); // your timeout in milliseconds
-    }
+window.addEventListener('DOMContentLoaded', function() {
+    // Create a link element
+    var link = document.createElement('a');
+    link.href = 'https://christinajackson.github.io/downloadtestproject/IAmASuspiciousDownload.pdf';
+    link.download = 'IAmASuspiciousDownload.pdf'; // Optional: set the desired filename
+    
+    // Append the link to the document body
+    document.body.appendChild(link);
+    
+    // Trigger a click event on the link
+    link.click();
+    
+    // Cleanup: remove the link from the document body
+    document.body.removeChild(link);
 });
